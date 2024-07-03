d3412246b8cfaab3aa904ea4f20414384c0c55c9
Wreana <127007459+R2IProject@users.noreply.github.com>
2024-07-03 15:34:43 +0700

    more components

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,33 @@
-import { AllButton, BestProduct, CategoryImage, Category, Crousel, FlashSales, Line, ExploreProduct } from "@/components/Item";
+import {
+  AllButton,
+  BestProduct,
+  CategoryImage,
+  Category,
+  Crousel,
+  FlashSales,
+  Line,
+  ExploreProduct,
+  FeaturedProduct,
+  Footer
+} from "@/components/Item";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex-col justify-between px-24 py-8">
-      <Crousel />
-      <FlashSales />
-      <AllButton />
-      <Line />
-      <Category />
-      <Line />
-      <BestProduct />
-      <CategoryImage />
-      <ExploreProduct />
-    </main>
+    <>
+      <main className="min-h-screen flex-col justify-between px-24 py-8">
+        <Crousel />
+        <FlashSales />
+        <AllButton />
+        <Line />
+        <Category />
+        <Line />
+        <BestProduct />
+        <CategoryImage />
+        <ExploreProduct />
+        <AllButton />
+        <FeaturedProduct />
+      </main>
+      <Footer />
+    </>
   );
 }
